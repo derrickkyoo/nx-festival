5432f3a3158bbf56ebf4521f0fa468931d1b8db1
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-11-23 07:48:51 -0700

    Update basic header e2e test

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -1,7 +1,11 @@
 describe('store-ui-shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&knob-title='));
+  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&knob-title=Nx Festival'));
 
   it('should render the component', () => {
     cy.get('nx-festival-header').should('exist');
+  });
+
+  it('should show the title', () => {
+    cy.get('nx-festival-header').contains('Nx Festival');
   });
 });
